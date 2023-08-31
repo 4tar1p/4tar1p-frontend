140d23c01942d655c6bd788a2fb7c52f47846701
hayoung225 <hayoung225@lguplus.co.kr>
2023-08-31 20:00:50 +0900

    [feat] 결제 디자인 변경

--- src/pages/Buying.js
+++ src/pages/Buying.js
@@ -1,9 +1,7 @@
 import { React, useState } from 'react';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
-import ProgressBar from 'components/ProgressBar';
 import RadiusRect from 'components/RadiusRect';
 import {
-  clickedNextStep,
   decreaseCnt,
   increaseCnt,
   selectedSports,
@@ -19,18 +17,6 @@ const SportsCard = ({ sports }) => {
     dispatch(selectedSports(sports.id));
   };
 
-  return (
-    <div className="sports_card" onClick={handleClick}>
-      <img className="sports_img" src={sports.imgSrc}></img>
-      <div className="sports_name">{sports.name}</div>
-      {sports.selected && <img src="/check_full.png"></img>}
-    </div>
-  );
-};
-
-const SelectedSports = ({ sports }) => {
-  const dispatch = useAppDispatch();
-
   const handleIncrease = () => {
     dispatch(increaseCnt(sports.id));
   };
@@ -38,30 +24,31 @@ const SelectedSports = ({ sports }) => {
   const handleDecrease = () => {
     dispatch(decreaseCnt(sports.id));
   };
+
   return (
-    <div>
-      <img src={sports.imgSrc}></img>
-      <div>{sports.name}</div>
-      <div>{sports.price}원</div>
-      <img
-        className="cnt_btn"
-        src={'/minus.png'}
-        onClick={handleDecrease}
-      ></img>
-      <div>{sports.cnt}</div>
-      <img className="cnt_btn" src={'/add.png'} onClick={handleIncrease}></img>
+    <div className="sports_card" onClick={handleClick}>
+        <img  className = "sports_img" src={sports.imgSrc}></img>
+        <div style={{fontSize:'2em', fontWeight:'bold'}}>{sports.name}</div>
+        <div style={{fontWeight:'bold', color:'red'}}>1회 {sports.price}원</div>
+        <div style={{fontWeight:'bold', color:'gray', textDecoration:'line-through'}}>1회 {sports.originPrice}</div>
+        <div className='increase-decrease-count'>
+          <img
+            className="cnt_btn"
+            src={'/minus.png'}
+            onClick={handleDecrease}
+          ></img>
+          <div>{sports.cnt}</div>
+          <img className="cnt_btn" src={'/add.png'} onClick={handleIncrease}></img>
+      </div>
     </div>
   );
 };
+
 const Buying = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const { step, sportsList } = useAppSelector((state) => state.buying);
-
-  const handleClick = () => {
-    dispatch(clickedNextStep(step + 1));
-  };
+  const { sportsList } = useAppSelector((state) => state.buying);
 
   const [isClick, setIsClick] = useState(false);
 
@@ -86,66 +73,23 @@ const Buying = () => {
 
   return (
     <div className="page">
-      <ProgressBar value={(100 * (step + 1)) / 3} />
-      {step == 0 && (
-        <>
-          <div className="progressTitle">어디서 운동하실건가요?</div>
-          <select>
-          {regionList.map((region, i) => {
-            return <option key={i}>{region.name}</option>;
-          })}
-          </select>
-
-          <RadiusRect
-            height="54px"
-            backgroundColor="#6adfa0"
-            color="white"
-            onClick={handleClick}
-          >
-            <div>다음</div>
-          </RadiusRect>
-        </>
-      )}
-      {step == 1 && (
-        <>
-          <div className="progressTitle">종목을 선택해주세요!</div>
-          <div className="sports_grid">
-            {sportsList.map((sports, i) => {
-              return <SportsCard key={i} sports={sports} />;
-            })}
-          </div>
-
-          <RadiusRect
-            height="54px"
-            backgroundColor="#6adfa0"
-            color="white"
-            onClick={handleClick}
-          >
-            <div>다음</div>
-          </RadiusRect>
-        </>
-      )}
-      {step == 2 && (
-        <>
-          <div className="progressTitle">횟수를 선택해주세요!</div>
-          {sportsList.map((sports, i) => {
-            if (sports.selected) {
-              return <SelectedSports key={i} sports={sportsList[i]} />;
-            }
-          })}
-          {isClick && <span className={spanClassName}>
-            *총 10회를 선택해주세요
-          </span>}
-          <RadiusRect
-            height="54px"
-            backgroundColor="#6adfa0"
-            color="white"
-            onClick={handlePay}
-          >
-            <div>결제</div>
-          </RadiusRect>
-        </>
-      )}
+      <div className="progressTitle">횟수를 선택해주세요!</div>
+      <div className="sports_card_wrapper" >
+        {sportsList.map((sports, i) => {
+            return <SportsCard key={i} sports={sportsList[i]} />;
+        })}
+        {isClick && <span className={spanClassName}>
+          *총 10회를 선택해주세요
+        </span>}
+      </div>
+      <RadiusRect
+        height="54px"
+        backgroundColor="#6adfa0"
+        color="white"
+        onClick={handlePay}
+      >
+        <div>결제</div>
+      </RadiusRect>
     </div>
   );
 };
