cc30285b736cc05d7d12899221d25f725e3f4028
LeeJuhae <leejuhae821@gmail.com>
2023-08-31 14:36:45 +0900

    [feat] 이용권 구매 페이지 초안 작성

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Route, Routes } from 'react-router-dom';
 import Home from 'pages/Home';
 import UI from 'pages/UI';
 import ApiTest from 'pages/ApiTest';
-
-const NotFound = React.lazy(() => import('./pages/NotFound'));
+import Buying from 'pages/Buying';
+import NotFound from 'pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path="/buying" element={<Buying />} />
       {/******** Example ********/}
       {/* <Route path="/login" element={<Login />} />
       <Route
